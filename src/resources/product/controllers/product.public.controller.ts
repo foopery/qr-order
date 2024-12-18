56de682b5dc0f2523d94881750c9a04a9de8e0e4
inhanbyeol <inhanbyeol94@limefriends.com>
2024-12-18 11:09:09 +0900

    feat: 상품 조회 API에 NotFound 응답 추가
    - @ApiNotFoundResponse 데코레이터를 상품 단일조회 메서드에 추가
    - 리소스가 존재하지 않을 경우 'No Resource' 응답을 명시적으로 정의

--- src/resources/product/controllers/product.public.controller.ts
+++ src/resources/product/controllers/product.public.controller.ts
@@ -7,6 +7,8 @@ import { PRODUCT_CONTROLLER_MESSAGE } from '@resources/product/product.constant'
 import { ApiController } from '@common/decorators/api-controller.decorator';
 import { ProductPublicFindListResponseDto } from '@resources/product/dto/response/product.public.find-list.response.dto';
 import { ProductPublicFindResponseDto } from '@resources/product/dto/response/product.public.find.response.dto';
+import { ApiNotFoundResponse } from '@nestjs/swagger';
+import { ApiResponseDto } from '@common/dto/response.dto';
 
 @ApiController('products')
 export class ProductPublicController {
@@ -21,6 +23,7 @@ export class ProductPublicController {
 
     @Get(':id')
     @ApiDetail('상품 단일조회', { isAuth: false })
+    @ApiNotFoundResponse({ description: 'No Resource', type: ApiResponseDto })
     async find(@Param('id', ParseIntPipe) id: number): Promise<ProductPublicFindResponseDto> {
         const resource = await this.publicService.findUniqueOrThrow(id);
         return apiResponse(PRODUCT_CONTROLLER_MESSAGE.FIND, resource);
