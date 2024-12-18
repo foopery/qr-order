f66fbbc7d4e1fc92defd2a488ebac644474ad8b7
inhanbyeol <inhanbyeol94@limefriends.com>
2024-12-18 11:21:04 +0900

    feat: 상품 조회 API에 NotFound 응답 추가
    - @ApiNotFoundResponse 데코레이터를 상품 단일조회 메서드에 추가
    - 리소스가 존재하지 않을 경우 'No Resource' 응답을 명시적으로 정의

--- src/resources/product/dto/response/product.public.find-list.response.dto.ts
+++ src/resources/product/dto/response/product.public.find-list.response.dto.ts
@@ -1,9 +1,11 @@
 import { ApiResponseDtoWithMeta } from '@common/dto/response.dto';
 import { OmitType } from '@nestjs/swagger';
 import { Product } from '@resources/product/models/product.model';
+import { AutoApiSchema } from '@common/decorators/auto.api-schema.decorator';
 
 export class ProductPublicFindListResponseDto extends ApiResponseDtoWithMeta {
     data: Data[];
 }
 
+@AutoApiSchema()
 export class Data extends OmitType(Product, []) {}
